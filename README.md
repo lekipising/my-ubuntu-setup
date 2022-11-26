448a3c4fe853ff5ec9d3b6c5bad1b54690bcc100
Liplan Lekipising <leeplanke@gmail.com>
2022-11-26 18:54:02 +0200

    create bash script for setting up machine

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # my-ubuntu-setup
+
+Contains ```setup.sh``` script to install all the packages I use on my Ubuntu machine.
+
---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# This script is used to setup a new ubuntu machine.
+
+# update system
+sudo apt update
+
+# upgrade system
+sudo apt upgrade -y && sudo apt dist-upgrade -y
+
+# install programs/tools - common
+sudo apt install -y git curl wget vlc gimp gparted gnome-tweaks gnome-shell-extensions neofetch nettools default-jre maven
+
+# install docker - steps
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+sudo mkdir -p /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
+
+# LAMP stack steps
+sudo apt install apache2
+sudo ufw allow in "Apache"
+
+sudo apt install mysql-server
+
+sudo mysql_secure_installation
+
+sudo apt install php libapache2-mod-php php-mysql
+
+# install snaps
+sudo snap install code --classic
+sudo snap install spotify
+sudo snap install postman
+sudo snap install slack --classic
+sudo snap install mysql-workbench-community
+
+# allow workbench to access password service
+sudo snap connect mysql-workbench-community:password-manager-service :password-manager-service
+
+# install nodejs - 18 LTS
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# create git config
+git config --global user.name "Liplan Lekipising"
+git config --global user.email "mail@me.com"
+
+# install google chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install ./google-chrome-stable_current_amd64.deb
+
+
